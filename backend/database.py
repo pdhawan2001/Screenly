6d697c14ad8a724f4dcc3010855584f6b0cdb236
Parth Dhawan <pdhawan2001@gmail.com>
2025-08-07 23:53:18 +0200

    working in gcp commit

--- backend/database.py
+++ backend/database.py
@@ -5,8 +5,6 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import logging
 
-# Configure logging
-logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Try to load environment variables from multiple possible locations
